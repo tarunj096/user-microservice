6d0b28a24ab4aa8f50871850782df01d726fb504
Tarun J <tarunj096@gmail.com>
2024-01-04 11:38:31 +0530

    Implemented JWT token creation

--- src/main/java/com/tarun/usermicroservice/services/AuthService.java
+++ src/main/java/com/tarun/usermicroservice/services/AuthService.java
@@ -7,7 +7,10 @@ import com.tarun.usermicroservice.models.SessionStatus;
 import com.tarun.usermicroservice.models.User;
 import com.tarun.usermicroservice.repositories.SessionRepository;
 import com.tarun.usermicroservice.repositories.UserRepository;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.MacAlgorithm;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,8 +18,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.MultiValueMapAdapter;
 
-import java.util.HashMap;
-import java.util.Optional;
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 @Service
 public class AuthService {
@@ -40,14 +44,33 @@ public class AuthService {
         User user = userOptional.get();
 
         if (!bCryptPasswordEncoder.matches(password, user.getPassword())) {
-            return null;
+            //throw an exception
+            throw new RuntimeException("Wrong password entered");
         }
+        //Generating a token
+        //String token = RandomStringUtils.randomAlphanumeric(30);
 
-        String token = RandomStringUtils.randomAlphanumeric(30);
+        MacAlgorithm alg = Jwts.SIG.HS256; //or HS384 or HS256
+        SecretKey key = alg.key().build();
+
+//        String message = "Hello World!";
+        Map<String,Object> jsonMap = new HashMap<>();
+        jsonMap.put("email",user.getEmail());
+        jsonMap.put("roles", List.of(user.getRoles()));
+        jsonMap.put("createdAt",new Date());
+        jsonMap.put("expiryAt", DateUtils.addDays(new Date(),30));
+        //byte[] content = message.getBytes(StandardCharsets.UTF_8);
+
+        // Create the compact JWS:
+        String jws = Jwts.builder().claims(jsonMap).signWith(key,alg).compact();
+        // Parse the compact JWS:
+        //content = Jwts.parser().verifyWith(key).build().parseSignedContent(jws).getPayload();
+
+        //assert message.equals(new String(content, StandardCharsets.UTF_8));
 
         Session session = new Session();
         session.setSessionStatus(SessionStatus.ACTIVE);
-        session.setToken(token);
+        session.setToken(jws);
         session.setUser(user);
         sessionRepository.save(session);
 
@@ -55,7 +78,7 @@ public class AuthService {
         userDto.setEmail(email);
 
         MultiValueMapAdapter<String, String> headers = new MultiValueMapAdapter<>(new HashMap<>());
-        headers.add(HttpHeaders.SET_COOKIE, "auth-token:" + token);
+        headers.add(HttpHeaders.SET_COOKIE, "auth-token:" + jws);
 
         ResponseEntity<UserDto> response = new ResponseEntity<>(userDto, headers, HttpStatus.OK);
 
@@ -97,5 +120,7 @@ public class AuthService {
 
         return SessionStatus.ACTIVE;
     }
+
+    //auth-token:eyJjdHkiOiJ0ZXh0L3BsYWluIiwiYWxnIjoiSFMyNTYifQ.SGVsbG8gV29ybGQh.z6Sw83W40CMfqZBvGv0cXstcWVIuRxVjRfUeHd9uWFY
 
 }
